a5c81f8b58da16f02303156aa4150e570ee97ff6
Maciej Gierej <makg@makg.eu>
2017-09-10 18:13:14 +0200

    Fixed package name

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-xgettext `find . -name \*.qml` -L JavaScript -o ./translations/plasma_applet_bitcoinprice.pot
+xgettext `find . -name \*.qml` -L JavaScript -o ./translations/plasma_applet_serverstatus.pot
